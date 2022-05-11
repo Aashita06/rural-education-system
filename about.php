8a0aadaa30e81220c20772de075a9bb965c161c8
Aashita06 <guptaaashita123@gmail.com>
2022-05-11 22:38:08 +0530

    1st commit

---
+++ about.php
@@ -0,0 +1,2 @@
+<h1><?php echo $title;?></h1>
+<p>Education is movement from darkness to light.</p>
---
+++ home.php
@@ -0,0 +1,17 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+
+</div>
+<div  class="col-lg-6">
+  
+  
+
+  
+</div>
+<div  class="col-lg-6">
+  
+
+</div>
+ 
+
